f106469d8bf7f96b0594d5d5af112503a5d9f424
miraniyam <ranz_@naver.com>
2017-08-14 18:21:00 +0900

    Add Acitivty and Layout for Control Trackers

---
+++ app/src/main/java/com/example/miranlee/mubser/CtrlTrackersActivity.java
@@ -0,0 +1,64 @@
+package com.example.miranlee.mubser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by KhawlaAlkooheji on 2/24/17.
+ */
+
+public class CtrlTrackersActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_QR_CODE=1000;
+
+    Button qr;
+    EditText user_code;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ctrltrackers);
+        setTitle("Control Trackers");
+        qr = (Button)findViewById(R.id.btn_qr);
+        user_code = (EditText)findViewById(R.id.user_code);
+    }
+
+    public void Click_qr(View v) {
+
+        Intent intent = new Intent(CtrlTrackersActivity.this, SimpleScannerActivity.class);
+
+        startActivityForResult(intent, REQUEST_CODE_QR_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        String code = data.getStringExtra("result");
+        user_code.setText(code);
+
+        switch (requestCode){
+            case REQUEST_CODE_QR_CODE:
+                if(resultCode==RESULT_OK){
+                    code = data.getStringExtra("result");
+                    user_code.setText(code);
+                }else {
+                    Toast.makeText(this, "Not Registered Code",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+    }
+
+    public void Click_done(View v) {
+
+    }
+
+
+}
--- app/src/main/java/com/example/miranlee/mubser/SettingsActivity.java
+++ app/src/main/java/com/example/miranlee/mubser/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.miranlee.mubser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by KhawlaAlkooheji on 2/24/17.
@@ -13,5 +15,10 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setTitle("Settings Page");
+    }
+
+    public void ClickCtrlT(View v) {
+        // 여기다가 사용자가 트랙커 지정해 줄 수 있는 페이지 만들기~
+        startActivity(new Intent(this, CtrlTrackersActivity.class));
     }
 }
